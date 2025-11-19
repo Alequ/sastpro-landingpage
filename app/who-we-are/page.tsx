69a597c4741d753643759c75803302a19f4333d0
Alex Turcu <turcu.alex5@gmail.com>
2025-11-19 15:04:03 +0200

    up

--- app/who-we-are/page.tsx
+++ app/who-we-are/page.tsx
@@ -653,7 +653,7 @@ export default function WhoWeArePage() {
                         className="text-white font-bold text-lg mb-1"
                         style={{ fontFamily: "var(--font-montserrat)" }}
                       >
-                        Marillis <span className="text-[#D0B970]">Kulamaa</span>
+                        Mariliis <span className="text-[#D0B970]">Kulamaa</span>
                       </h3>
                       <p
                         className="text-gray-300 text-sm"
